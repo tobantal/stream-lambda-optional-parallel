bea38d9960d530c5473ad442d763b471c2d154f8
Тоболкин Антон Александрович <aatobolkin@ois.ru>
2018-11-28 16:31:37 +0700

    added PredicateAndConsumerExample

--- src/main/java/com/da/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/main/java/com/da/functionalInterfaces/PredicateAndConsumerExample.java
@@ -1,4 +1,35 @@
 package com.da.functionalInterfaces;
 
+import com.da.data.Student;
+import com.da.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class PredicateAndConsumerExample {
+
+    private static final Predicate<Student> p1 = s -> s.getGradeLevel()>=3;
+    private static final Predicate<Student> p2 = s -> s.getGpa()>=3.9;
+    private static final BiConsumer<String, List<String>> biConsumer = (name, activities) -> {
+        System.out.printf("%s: %s\n", name, activities);
+    };
+
+    static final Consumer<Student> studentConsumer = student -> {
+        if(p1.and(p2).test(student)) {
+            biConsumer.accept(student.getName(), student.getActivities());
+        }
+    };
+
+    static void printNameAndActivities() {
+        StudentDataBase
+                .getAllStudents()
+                .forEach(student -> studentConsumer.accept(student));
+    }
+
+    public static void main(String[] args) {
+        printNameAndActivities();
+
+    }
 }
