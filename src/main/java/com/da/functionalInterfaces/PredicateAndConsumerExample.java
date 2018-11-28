3f1db3f2ca6346b337a583622728241dce735b7a
Тоболкин Антон Александрович <aatobolkin@ois.ru>
2018-11-28 16:41:45 +0700

    refactored PredicateAndConsumerExample

--- src/main/java/com/da/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/main/java/com/da/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,21 +5,30 @@ import com.da.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class PredicateAndConsumerExample {
 
-    private static final Predicate<Student> p1 = s -> s.getGradeLevel()>=3;
-    private static final Predicate<Student> p2 = s -> s.getGpa()>=3.9;
+    //private static final Predicate<Student> p1 = s -> s.getGradeLevel()>=3;
+    //private static final Predicate<Student> p2 = s -> s.getGpa()>=3.9;
+
+    private static final BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpa) -> gradeLevel>=3 && gpa>=3.9;
+
     private static final BiConsumer<String, List<String>> biConsumer = (name, activities) -> {
         System.out.printf("%s: %s\n", name, activities);
     };
 
-    static final Consumer<Student> studentConsumer = student -> {
+    private static final Consumer<Student> studentConsumer = student -> {
+        if(biPredicate.test(student.getGradeLevel(), student.getGpa())) {
+            biConsumer.accept(student.getName(), student.getActivities());
+        }
+        /*
         if(p1.and(p2).test(student)) {
             biConsumer.accept(student.getName(), student.getActivities());
         }
+        */
     };
 
     static void printNameAndActivities() {
